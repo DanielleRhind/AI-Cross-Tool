342479c9c9036f3f61cab75909fde987f217ff17
Danielle Rhind <danielle.rhind@monolithicpower.com>
2025-08-20 14:18:22 -0700

    Delete playwright.py

--- playwright.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import sys
-
-def install_playwright():
-    subprocess.run([sys.executable, "-m", "playwright", "install"], check=True)
-
-if __name__ == "__main__":
-    install_playwright()
