6b9e8661b8f26871bf8439fe701a9bf0daf5f7cf
Danielle Rhind <danielle.rhind@monolithicpower.com>
2025-08-20 12:16:25 -0700

    Create install_playwright.sh

---
+++ install_playwright.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# scripts/install_playwright.sh
+
+# This installs the Chromium/Firefox/ WebKit binaries that
+# the Python Playwright package expects.
+python -m playwright install
